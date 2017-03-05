f404358d5e6a5e9555ebb6b5e29916b9f9a721d7
Hernan Bessera <hernanbessera@MacBook-Air-de-Hernan.local>
2017-03-05 12:38:31 -0300

    cambio en font

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,22 @@ var gulp = require('gulp'),
 		sass = require('gulp-sass'),
 		rename = require('gulp-rename'),
     uglify = require('gulp-uglify'),
-		cleanCss = require('gulp-clean-css');
+		cleanCss = require('gulp-clean-css'),
+		browserSync = require('browser-sync').create();
 
 gulp.task('uglify', function(){
 	gulp.src('./js/index.js')
 	.pipe(uglify())
 	.pipe(rename({ suffix: '.min'}))
 	.pipe(gulp.dest('./js/'))
+	.pipe(browserSync.stream());
 });
 
 gulp.task('sass', function(){
 	return gulp.src('./css/style.sass')
 	.pipe(sass().on('error', sass.logError))
-	.pipe(gulp.dest('./css/'));
+	.pipe(gulp.dest('./css/'))
+	.pipe(browserSync.stream());
 });
 
 gulp.task('minify-css', function(){
@@ -30,4 +33,13 @@ gulp.task('watch', function(){
 	gulp.watch('./css/style.css', ['minify-css']);
 });
 
-gulp.task('default', ['uglify', 'sass', 'minify-css']);
+gulp.task('serve', function(){
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
+  gulp.watch('*.html').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['uglify', 'sass', 'minify-css','watch', 'serve']);
